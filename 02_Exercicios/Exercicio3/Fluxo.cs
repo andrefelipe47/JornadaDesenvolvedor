b18c480ca2d189795befca8b851506586b471f3b
Andre Martins <andre.martins@rumosolucoes.com>
2023-01-09 11:36:41 -0300

    Inclusão do exercicio 3 da segunda leva de exercicios

---
+++ 02_Exercicios/Exercicio3/Fluxo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public class Fluxo
+    {
+        public void ExecutarFluxo()
+        {
+            var pessoas = new List<Pessoa>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Qual o nome da pessoa?");
+                string nomePessoa = Console.ReadLine();
+
+                Console.WriteLine("Qual a idade da pessoa?");
+                short idadePessoa = Convert.ToInt16(Console.ReadLine());
+
+                var pessoa = new Pessoa();
+                pessoa.Nome = nomePessoa;
+                pessoa.Idade = idadePessoa;
+
+                pessoas.Add(pessoa);
+            }
+
+            var nomePessoaMaisVelha = "";
+            var ultimaIdadeMaisVelho = -1;
+
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine("Nome: " + pessoa.Nome + " Idade: " + pessoa.Idade);
+
+                if (ultimaIdadeMaisVelho < pessoa.Idade)
+                {
+                    ultimaIdadeMaisVelho = pessoa.Idade;
+                    nomePessoaMaisVelha = pessoa.Nome;
+                }
+            }
+
+            Console.WriteLine("O mais velho de todos: " + nomePessoaMaisVelha);
+        }
+    }
+}
---
+++ 02_Exercicios/Exercicio3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fluxo = new Fluxo();
+            fluxo.ExecutarFluxo();
+        }
+    }
+}
