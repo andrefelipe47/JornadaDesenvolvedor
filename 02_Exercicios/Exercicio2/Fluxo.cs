e7cc7facba9e4869d98cb5786559563ba4ddb990
Andre Martins <andre.martins@rumosolucoes.com>
2023-01-10 10:00:16 -0300

    Add Exercicio 2 da segunda leva de exercícios

---
+++ 02_Exercicios/Exercicio2/Fluxo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    internal class Fluxo
+    {
+        public void Executar()
+        {
+            var numeros = new List<int>();
+            var totalNumeros = 15;
+
+            for (int i = 0; i < totalNumeros; i++)
+            {
+                Console.WriteLine("Escolha um número inteiro: ");
+                numeros.Add(Convert.ToInt32(Console.ReadLine()));
+                Console.Clear();
+                Console.WriteLine($"Foi inserido o {i + 1}º numero de {totalNumeros} números.");
+            }
+
+            foreach (var numero in numeros)
+            {
+                if (numero > 0)
+                    Console.WriteLine("O número " + numero + " se trata de um número positivo.");
+            }
+        }
+    }
+}
---
+++ 02_Exercicios/Exercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fluxo fluxo = new Fluxo();
+            fluxo.Executar();
+        }
+    }
+}
