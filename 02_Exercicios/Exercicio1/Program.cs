7dd7d9b9a17459ebe3a5283111393f43d645dc8a
Andre Martins <andre.martins@rumosolucoes.com>
2023-01-10 10:21:02 -0300

    Incluisão primeiro exercicio de vetores e matrizes na segunda leva de exercicios

---
+++ 02_Exercicios/Exercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("######################################");
+            Console.WriteLine("Sistema para calcular médias de provas");
+            Console.WriteLine("######################################");
+            Console.WriteLine(Environment.NewLine + Environment.NewLine);
+
+            var notas = new List<decimal>();
+            while (true)
+            {
+                Console.WriteLine("Digite uma nota de prova, ou escreva PARE para finalizar o cadastro das notas.");
+                var respostaUsuario = Console.ReadLine();
+
+                if(respostaUsuario.Trim().ToUpper() == "PARE")
+                    break;
+
+                notas.Add(Convert.ToDecimal(respostaUsuario));
+            }
+
+            decimal somaNotas = 0;
+            foreach (var nota in notas)
+            {
+                somaNotas += nota;
+                // e a mesma coisa de usar
+                //somaNotas = somaNotas + nota;
+            }
+
+            Console.WriteLine("A média da turma foi: " + somaNotas / notas.Count());
+        }
+    }
+}
