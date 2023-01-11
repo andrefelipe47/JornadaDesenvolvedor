248b0bcebf6c1adc553e8fe577a7121a3534243e
Andre Martins <andre.martins@rumosolucoes.com>
2023-01-11 09:56:27 -0300

    Leitura arquivos texto

---
+++ Exemplos/ComoManipularArquivosTexto/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ComoLerArquivoTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader($"C:{Path.DirectorySeparatorChar}Projetos{Path.DirectorySeparatorChar}Entradas{Path.DirectorySeparatorChar}Sample.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
