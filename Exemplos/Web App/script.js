1011c1e6a2ec14eb87123505a00128706c0c102f
Andre Martins <andre.martins@rumosolucoes.com>
2023-01-24 11:44:39 -0300

    Inclusao dados tabelas e tratamento de null

--- Exemplos/Web App/script.js
+++ Exemplos/Web App/script.js
@@ -21,8 +21,21 @@ function ListarClientes() {
 
 function ConstruirTabela(linhas) {
 
+    var htmlTabela = '';
+
     $(linhas).each(function (index, linha) {
         console.log(linha);
+
+        htmlTabela = htmlTabela + `<tr><th>${linha.cpfCliente}</th><td>${linha.nome}</td><td>${linha.nascimento}</td><td>${FormatarTelefone(linha.telefone)}</td></tr>`;
     });
 
+    $('#corpoTabelaCliente').html(htmlTabela);
+}
+
+function FormatarTelefone(texto) {
+    if (texto == null) {
+        return "";
+    } else {
+        return texto;
+    }
 }
