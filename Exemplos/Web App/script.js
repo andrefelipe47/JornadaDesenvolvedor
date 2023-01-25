9a9042a6b8af7dd16dd091e97d9f9103c191b0ff
Andre Martins <andre.martins@rumosolucoes.com>
2023-01-25 09:57:50 -0300

    Finalizacao listagem clientes

--- Exemplos/Web App/script.js
+++ Exemplos/Web App/script.js
@@ -1,7 +1,12 @@
+$(document).ready(function() {
+    ListarClientes();
+});
+
 var urlBaseApi = "https://localhost:44349";
 
+
 function LimparCorpoTabelaClientes() {
-    var componenteSelecionado = $('#corpoTabelaCliente');
+    var componenteSelecionado = $('#tabelaCliente tbody');
     componenteSelecionado.html('');
 }
 
@@ -24,18 +29,35 @@ function ConstruirTabela(linhas) {
     var htmlTabela = '';
 
     $(linhas).each(function (index, linha) {
-        console.log(linha);
-
-        htmlTabela = htmlTabela + `<tr><th>${linha.cpfCliente}</th><td>${linha.nome}</td><td>${linha.nascimento}</td><td>${FormatarTelefone(linha.telefone)}</td></tr>`;
+        htmlTabela = htmlTabela + `<tr><th>${linha.cpfCliente}</th><td>${linha.nome}</td><td>${FormatarData(linha.nascimento)}</td><td>${FormatarTelefone(linha.telefone)}</td></tr>`;
     });
 
-    $('#corpoTabelaCliente').html(htmlTabela);
+    $('#tabelaCliente tbody').html(htmlTabela);
+    $('#tabelaCliente').DataTable({
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json'
+        }
+    });
 }
 
 function FormatarTelefone(texto) {
     if (texto == null) {
         return "";
     } else {
-        return texto;
+
+        var ddd = texto.slice(0,2);
+        var primeiroNumero = texto.slice(2,7);
+        var segundoNumero = texto.slice(7,11);
+
+        return `(${ddd}) ${primeiroNumero}-${segundoNumero}`;
     }
+}
+
+function FormatarData(dataString){
+    var dataTeste = new Date(dataString);
+    var ano = dataTeste.getFullYear();
+    var mes = String(dataTeste.getMonth() + 1).length == 1 ? '0' + String(dataTeste.getMonth() + 1) : String(dataTeste.getMonth() + 1);
+    var dia = String(dataTeste.getDate()).length == 1 ? '0' + String(dataTeste.getDate()) : String(dataTeste.getDate());
+
+    return dia + "/" + mes + "/" + ano;
 }
